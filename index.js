27da2eddfac4e36caa4019134c0d19d40f8e9181
harroud omar <harroud.omar.dev@gmail.com>
2024-07-29 20:24:43 +0100

    changed index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 console.log('we added a js file')
+
+class Person {
+
+    constructor(name){
+      this.name = name;
+    }
+
+    greet(){
+       console.log(this.name)
+    }
+}
+// function Person(name){
+//      this.name = name;
+// }
+// Person.prototype.greet = function(){
+//     console.log(this.name+" is user")
+// }
+
+const person = new Person('omar')
+console.log(Object.getPrototypeOf(person), Person.prototype)  // this returns { greet: [Function (anonymous)] }
